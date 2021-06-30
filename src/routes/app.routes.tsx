8fada1584f8631b41127fc4548ce3940703f9ee5
Hosana Barcelos <55986782+hosanabarcelos@users.noreply.github.com>
2021-06-30 19:28:01 -0300

    Add files via upload

---
+++ src/routes/app.routes.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import { theme } from '../global/styles/theme';
+
+import { Home } from '../pages/Home';
+import { AppointmentDetails } from '../pages/AppointmentDetails';
+import { AppointmentCreate } from '../pages/AppointmentCreate';
+
+const { Navigator, Screen } = createStackNavigator();
+
+export function AppRoutes() {
+  return(
+    <Navigator
+      headerMode="none"
+      screenOptions={{
+        cardStyle: {
+          backgroundColor: theme.colors.secondary100
+        }
+      }}
+    >     
+      <Screen 
+        name="Home"
+        component={Home}
+      />
+      <Screen 
+        name="AppointmentDetails"
+        component={AppointmentDetails}
+      />
+      <Screen 
+        name="AppointmentCreate"
+        component={AppointmentCreate}
+      />      
+    </Navigator>
+  )
+}
